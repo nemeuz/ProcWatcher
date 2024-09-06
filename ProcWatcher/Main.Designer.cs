9f8cf85be46a6ae1e44816e95d225732751920b8
MG <mg.me.dev@outlook.com>
2024-09-06 11:59:36 +0700

    Rename main form and set its resolution

--- ProcWatcher/Main.Designer.cs
+++ ProcWatcher/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProcWatcher
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Main
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(484, 261);
+            Font = new Font("Segoe UI", 10F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ProcWatcher";
+            ResumeLayout(false);
         }
 
         #endregion
--- ProcWatcher/Main.cs
+++ ProcWatcher/Main.cs
@@ -1,8 +1,8 @@
 namespace ProcWatcher
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
